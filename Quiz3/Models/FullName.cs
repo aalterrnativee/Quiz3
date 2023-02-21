43e32d500fa0bdf4f4151b51448ebd11c808992a
aalterrnativee <alternative3004@gmail.com>
2023-02-21 18:22:59 +0330

    Add project files.

---
+++ Quiz3/Models/FullName.cs
@@ -0,0 +1,8 @@
+﻿namespace Quiz3.Models
+{
+    public class FullName
+    {
+        public string Name { get; set; } = "علیرضا";
+        public string Family { get; set; } = "علوی";
+    }
+}
---
+++ Quiz3/Models/Services/FullNameService.cs
@@ -0,0 +1,12 @@
+﻿namespace Quiz3.Models.Services
+{
+    public class FullNameService
+    {
+        FullName fullName = new FullName();
+
+        public string PrintFullName()
+        {
+            return fullName.Name + " " + fullName.Family;
+        }
+    }
+}
